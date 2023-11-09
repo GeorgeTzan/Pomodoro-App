940f7ec1f12be441b70a9e174ce2608aa0dd42c8
GeorgeTzan` <int02846@uoi.gr>
2023-11-09 20:45:43 +0200

    - Added icon

--- App.cpp
+++ App.cpp
@@ -12,7 +12,10 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit() {
 	wxInitAllImageHandlers();
 
-	MainFrame* mainFrame = new MainFrame("Pomodoro");
+	wxIcon icon("tomato.ico", wxBITMAP_TYPE_ICO);	
+
+	MainFrame* mainFrame = new MainFrame("Pomodoro by Zenullari Konstandinos & Tzanopoulos Georgios");
+	mainFrame->SetIcons(icon);
 	wxSetEnv("QT_QPA_PLATFORM", "minimal");
 
 	mainFrame->SetClientSize(1000, 600);
